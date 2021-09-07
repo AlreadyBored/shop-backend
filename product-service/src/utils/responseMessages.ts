2f4b22132aaea75af7258ad47ae7ea2b9921f9ea
Maksim Shylau <shilaums@gmail.com>
2021-09-08 01:04:49 +0300

    refactor: unify exports style

--- product-service/src/utils/responseMessages.ts
+++ product-service/src/utils/responseMessages.ts
@@ -1,11 +1,7 @@
-const getSingleProductBadRequestMessage = id => `You have to provide correct id instead of ${id}`;
-const getNotFoundMessage = id => `Product with ${id} is not found`;
-const getInternalServerErrorMessage = err => `Something went wrong on server side: ${err}`;
-const addProductBadRequestMessage = `Invalid requests body`;
+export const getSingleProductBadRequestMessage = id => `You have to provide correct id instead of ${id}`;
 
-export {
-    addProductBadRequestMessage,
-    getSingleProductBadRequestMessage,
-    getNotFoundMessage,
-    getInternalServerErrorMessage
-};
+export const getNotFoundMessage = id => `Product with ${id} is not found`;
+
+export const getInternalServerErrorMessage = err => `Something went wrong on server side: ${err}`;
+
+export const addProductBadRequestMessage = `Invalid requests body`;
