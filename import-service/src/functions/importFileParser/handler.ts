36771957d75349a71252a6cb39a179b188807c04
Maksim Shylau <shilaums@gmail.com>
2021-09-19 21:33:43 +0300

    feat: implement importFileParser

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register';
 import { buildResponse } from '@libs/apiGateway';
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { middyfy } from '@libs/lambda';
-import { BUCKET_NAME, REGION, STATUS_CODES, getInternalServerErrorMessage } from '../../utils/constants';
+import { BUCKET_NAME, REGION, STATUS_CODES, getInternalServerErrorMessage, UPLOAD_PREFIX, PARSE_PREFIX } from '../../utils/constants';
 import AWS from 'aws-sdk';
 import cvs from 'csv-parser';
 
@@ -13,11 +13,11 @@ const importFileParser = async (event): Promise<APIGatewayProxyResult> => {
     const { Records } = event;
 
     Records.forEach(record => {
-      const { key } = record.s3.object;
+      const { key: fileName } = record.s3.object;
 
       const params = {
         Bucket: BUCKET_NAME,
-        Key: key
+        Key: fileName
       };
 
       const s3Bucket = new AWS.S3({ region: REGION });
@@ -27,16 +27,25 @@ const importFileParser = async (event): Promise<APIGatewayProxyResult> => {
       const s3ReadableStream = s3Object.createReadStream();
 
       s3ReadableStream.pipe(cvs())
-        .on('data', () => {
-
+        .on('data', (chunk) => {
+          console.log(`Recieved part of data ${chunk.toString()}`);
         })
-        .on('error', () => {
-
+        .on('error', (e) => {
+          throw new Error(`Error occured: ${e}`);
         })
-        .on('end', () => {
+        .on('end', async () => {
+          const sourceName = `${BUCKET_NAME}/${fileName}`;
+          const destName = sourceName.replace(UPLOAD_PREFIX, PARSE_PREFIX);
 
+          const copyParams = {
+            Bucket: BUCKET_NAME,
+            CopySource: sourceName,
+            Key: destName
+          };
+
+          await s3Bucket.copyObject(copyParams).promise();
+          await s3Bucket.deleteObject(params).promise();
         });
-
     });
 
     return buildResponse(ACCEPTED, { message: 'Successfully parsed' });
@@ -44,6 +53,6 @@ const importFileParser = async (event): Promise<APIGatewayProxyResult> => {
     const message = getInternalServerErrorMessage(e);
     return buildResponse(INTERNAL_SERVER_ERROR, { message });
   }
-}
+};
 
 export const main = middyfy(importFileParser);
