3b85da8974a48092d1759ca18d71af7d7379abd9
Maksim Shylau <shilaums@gmail.com>
2021-10-16 16:55:21 +0300

    refactor: remove unused code

--- authorization-service/src/utils/constants.ts
+++ authorization-service/src/utils/constants.ts
@@ -1,17 +1,3 @@
-export enum STATUS_CODES {
-    OK = 200,
-    ACCEPTED = 202,
-    BAD_REQUEST = 400,
-    NOT_FOUND = 404,
-    INTERNAL_SERVER_ERROR = 500,
-    UNAUTHORIZED = 401
-};
-
-export const CORS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
-};
-
 export const REGION = 'eu-west-1';
 
 export const getInternalServerErrorMessage = err => `Something went wrong on server side: ${err}`;
