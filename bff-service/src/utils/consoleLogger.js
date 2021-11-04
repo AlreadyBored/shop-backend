28547e144cb579ddaa00a31c1edb38d5049fffb1
Maksim Shylau <shilaums@gmail.com>
2021-11-04 21:53:38 +0300

    feat: replace express with nest

--- bff-service/src/utils/consoleLogger.js
+++
@@ -1,9 +0,0 @@
-const logRequest = (requestData) => {
-    for (let [key, data] of Object.entries(requestData)) {
-        console.log(`[LOG] Request's ${key} is ${JSON.stringify(data)}`);
-    }
-};
-
-module.exports = {
-    logRequest
-};
