6657c7953d3ebb9727b26f6a051ec87ad7df014c
Maksim Shylau <shilaums@gmail.com>
2021-11-05 20:40:01 +0300

    refactor: place code from bff to app module

--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BffModule } from './bff/bff.module';
-import { BffController } from './bff/bff.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BffModule],
-  controllers: [AppController, BffController],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
